01624be999917cad46ffd0d30a8b0b360665cce8
shakirat <shakiratganiyy@gmail.com>
2022-09-30 13:13:02 +0100

    Added the search engine

--- src/app.js
+++ src/app.js
@@ -44,8 +44,17 @@ function displayTemperature(response) {
   iconElement.setAttribute("alt", response.data.weather[0].description);
 }
 
-let apiKey = "e1a1e984ddfbd4c270b900f1212c4a45";
-let city = "Nigeria";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+function search(city) {
+  let apiKey = "e1a1e984ddfbd4c270b900f1212c4a45";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayTemperature);
+}
 
-axios.get(apiUrl).then(displayTemperature);
+function displaySubmit(event) {
+  event.preventDefault();
+  let cityInputElement = document.querySelector("#city-input");
+  search(cityInputElement.value);
+}
+
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", displaySubmit);
